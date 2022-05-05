832136c97e233509d5fea658624e38d8dff3dab5
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-05 12:16:11 +0530

    [Harshal]Add:To Check Frequency Of Words in a Paragraph

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -1,24 +1,23 @@
 ﻿using HashTables;
 namespace HoshTables
 {
-
     class program
     {
-
         static void Main(string[] args)
         {
             MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
-            string words = "to be or not to be";
+
+            string words = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             string[] arr = words.Split(' ');
             LinkedList<string> checkForDuplication = new LinkedList<string>();
-            foreach (string element in arr) // to -> be
+            foreach (string element in arr)
             {
                 int count = 0;
-                foreach (string match in arr) //to->be->or->not->to-> be-> 
+                foreach (string match in arr)
                 {
                     if (element == match)
                     {
-                        count++;//1->2
+                        count++;
                         if (checkForDuplication.Contains(element))
                         {
                             break;
@@ -27,14 +26,47 @@ namespace HoshTables
 
                 }
 
-                if (!checkForDuplication.Contains(element))
+                if (checkForDuplication.Contains(element))
                 {
-                    checkForDuplication.AddLast(element);
-                    hash.Add(element, count);//(to,2)
+                    continue;
                 }
+                checkForDuplication.AddLast(element);
+                hash.Add(element, count);
             }
-            hash.Display();
+            int freq = hash.Get("avoidable");
+            Console.WriteLine("Frequency of the word Avoidable: " + freq);
+            hash.Remove("avoidable");
+            freq = hash.Get("avoidable");
+            Console.WriteLine("Frequency of the word Avoidable after removing: " + freq);
         }
+        //MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+        //string words = "to be or not to be";
+        //string[] arr = words.Split(' ');
+        //LinkedList<string> checkForDuplication = new LinkedList<string>();
+        //foreach (string element in arr) // to -> be
+        //{
+        //    int count = 0;
+        //    foreach (string match in arr) //to->be->or->not->to-> be-> 
+        //    {
+        //        if (element == match)
+        //        {
+        //            count++;//1->2
+        //            if (checkForDuplication.Contains(element))
+        //            {
+        //                break;
+        //            }
+        //        }
+
+        //    }
+
+        //    if (!checkForDuplication.Contains(element))
+        //    {
+        //        checkForDuplication.AddLast(element);
+        //        hash.Add(element, count);//(to,2)
+        //    }
+        //}
+        //hash.Display();
+
     }
 }
 								
